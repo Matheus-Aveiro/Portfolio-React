99b9ccf08d98ed7a39015923724b284d7ff95efe
Matheus Aveiro <134419298+Aveiromat@users.noreply.github.com>
2024-11-22 16:28:45 -0300

    🎉 init: Commit inicial
    Recommit de projeto vinculado à conta antiga.

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/matheus-aveiro.png" />
+
+export default Avatar
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,27 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
+  <aside>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Matheus Aveiro</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Matheus-Aveiro
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Full-Stack
+      </Descricao>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
+    </SidebarContainer>
+  </aside>
+)
+
+export default Sidebar
