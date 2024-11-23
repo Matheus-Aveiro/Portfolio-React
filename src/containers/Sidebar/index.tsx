188718585404f5ba1cca0da8d6f7ca459cbebbca
Matheus Aveiro <134419298+Aveiromat@users.noreply.github.com>
2024-11-23 11:37:43 -0300

    💄feat: Estilização
    Divisão de Grid; Remoção de conteúdo;

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,5 +1,4 @@
 import Avatar from '../../components/Avatar'
-import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
 import { Descricao, BotaoTema, SidebarContainer } from './styles'
@@ -13,9 +12,6 @@ const Sidebar = (props: Props) => (
     <SidebarContainer>
       <Avatar />
       <Titulo fontSize={20}>Matheus Aveiro</Titulo>
-      <Paragrafo tipo="secundario" fontSize={16}>
-        Matheus-Aveiro
-      </Paragrafo>
       <Descricao tipo="principal" fontSize={12}>
         Engenheiro Full-Stack
       </Descricao>
